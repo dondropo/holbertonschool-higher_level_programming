c352e7c63d8084c0bde5c6fc44dd366d88b46aef
dondropo <alejandroruscamoreno@gmail.com>
2020-04-27 22:26:49 +0000

    Adding task 8, v1

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpireted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + ' ' + "with Python"
+print(str)
