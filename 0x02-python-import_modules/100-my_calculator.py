874a9241fd7c62834b4be77272db37f2f91add68
dondropo <alejandroruscamoreno@gmail.com>
2020-05-01 04:20:55 +0000

    Advanced task 100, v2: exit stat and operator condition corrected

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,20 +3,21 @@ if __name__ == '__main__':
     from calculator_1 import add, sub, mul, div
     from sys import argv
 
-if len(argv) != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    exit(1)
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
     n1 = int(argv[1])
+    op = argv[2]
     n3 = int(argv[3])
 
-    if argv[2] is "+":
+    if op is "+":
         print("{:d} + {:d} = {:d}".format(n1, n3, add(n1, n3)))
-    elif argv[2] is "-":
+    elif op is "-":
         print("{:d} - {:d} = {:d}".format(n1, n3, add(n1, n3)))
-    elif argv[2] is "*":
+    elif op is "*":
         print("{:d} * {:d} = {:d}".format(n1, n3, add(n1, n3)))
-    elif argv[2] is "/":
+    elif op is "/":
         print("{:d} / {:d} = {:d}".format(n1, n3, add(n1, n3)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-    exit(1)
+        exit(1)
