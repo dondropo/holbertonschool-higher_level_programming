6af23e426324b1dddef301f8d59c0f45dcfe9eab
dondropo <alejandroruscamoreno@gmail.com>
2020-05-05 23:39:57 +0000

    Task 5, fixed logic

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    for i in range(len(my_string)-2):
-        if my_string[i] == "c" or my_string[i] == "C":
-            my_string = my_string[:i] + "" + my_string[i + 1:]
-    return my_string
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] != "c" and my_string[i] != "C":
+            new_string += my_string[i]
+    return new_string
