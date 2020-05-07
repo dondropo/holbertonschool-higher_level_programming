ba557855fba6425b9d0e4358948ad54c4c8c0939
dondropo <alejandroruscamoreno@gmail.com>
2020-05-07 21:14:36 +0000

    task 11, all the code inside the return

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    new = []
-    support_list = list(map(lambda x: x * number, my_list))
-    new.append(support_list)
-    return(new)
+    return(list(map(lambda x: x * number, my_list)))
