68baeaecec7002b4338880b40fb63263a5a41b02
dondropo <alejandroruscamoreno@gmail.com>
2020-05-23 02:45:01 +0000

    Adding task 2 with its test

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """
+    >>> matrix_divided([[1.0, 3.1], [4.3, -5.3, -6.2]], 1)
+    Traceback (most recent call last):
+    TypeError: Each row of the matrix must have the same size
+    """
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    new_mtx = []
+    for column in matrix:
+        if len(column) is not len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        tmp_list = list(map(lambda row: round(row / div, 2), column))
+        new_mtx.append(tmp_list)
+
+    return new_mtx
