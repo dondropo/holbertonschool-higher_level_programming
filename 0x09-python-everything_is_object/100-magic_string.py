40494f1d328e911fc96d91af94acd13b927feb75
dondropo <alejandroruscamoreno@gmail.com>
2020-05-26 20:58:39 +0000

    Adding tasks from 0 to advance 100

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+print magic string
+"""
+
+
+def magic_string(a=[]):
+    """#pythonic"""
+
+    a.append("Holberton")
+    return ", ".join(a)
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+copy list
+"""
+
+
+def copy_list(l):
+    """returns the copy of l"""
+    return(l[:])
