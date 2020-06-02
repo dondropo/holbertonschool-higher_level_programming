17ffaa4f53ed609f760d6aecd6f380b9dae3801e
dondropo <alejandroruscamoreno@gmail.com>
2020-06-02 21:54:25 +0000

    Turn to private height and width

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -11,9 +11,9 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         self.integer_validator("width", width)
-        self.width = width
+        self.__width = width
         self.integer_validator("height", height)
-        self.height = height
+        self.__height = height
 
     def area(self):
         return self.__height * self.__width
