6130c62868d95f1bdac5879cb253919d9fd03834
dondropo <alejandroruscamoreno@gmail.com>
2020-06-02 18:11:13 +0000

    Fixed task 4 with negative instance condition

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,9 +7,9 @@ from the specified class; otherwise False
 
 
 def inherits_from(obj, a_class):
-    if type(obj) is a_class:
+    if type(obj) is not a_class:
         return True
-    elif isinstance(obj, a_class):
+    if not isinstance(obj, a_class):
         return True
     else:
         return False
