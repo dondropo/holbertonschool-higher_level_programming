78a16370cd8ec516046b87e54592684983be3548
dondropo <alejandroruscamoreno@gmail.com>
2020-06-02 23:54:14 +0000

    Refix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,7 +9,4 @@ from the specified class; otherwise False
 def inherits_from(obj, a_class):
     """checks obj"""
 
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    else:
-        return False
+    return issubclass(type(obj), a_class) and type(obj) != a_class
