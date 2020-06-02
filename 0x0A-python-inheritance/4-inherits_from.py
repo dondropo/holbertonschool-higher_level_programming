e262a689225cf9e80b53bcc1d7eab67916d15af1
dondropo <alejandroruscamoreno@gmail.com>
2020-06-02 23:30:54 +0000

    Corrected obj condition

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,9 +9,7 @@ from the specified class; otherwise False
 def inherits_from(obj, a_class):
     """checks obj"""
 
-    if type(obj) is not a_class:
-        return True
-    if not isinstance(obj, a_class):
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     else:
         return False
