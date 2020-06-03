8681a96313a112378fc7e48f4f0a7c137f8a869c
dondropo <alejandroruscamoreno@gmail.com>
2020-06-03 00:33:55 +0000

    issubclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,7 +7,7 @@ instance of a class that inherited
 
 def inherits_from(obj, a_class):
     """inheritance class"""
-    if isinstance(type(obj), a_class) and type(obj) != a_class:
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
         return False
