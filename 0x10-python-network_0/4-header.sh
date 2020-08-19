163bf4060162ae426a77b470fbb5c5eb82400ac9
dondropo <alejandroruscamoreno@gmail.com>
2020-08-19 02:32:23 +0000

    [Fix]: deleted 2nd comment line and space

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Takes in a URL as an argument, sends a GET request to the URL,
-# and displays the body of the response
-
+# Takes in a URL as an argument, sends a GET request to the URL
 curl -sbL -X GET -H "X-HolbertonSchool-User-Id: 98" "$1"
