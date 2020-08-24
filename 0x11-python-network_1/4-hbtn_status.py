922cb85838e33c8115600cb547d14ae55a866811
dondropo <alejandroruscamoreno@gmail.com>
2020-08-24 21:53:37 +0000

    [Fix]: got the correct output

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,5 +5,5 @@ import requests as rq
 
 response = rq.get('https://intranet.hbtn.io/status')
 print('Body response:')
-print('\t- type: {}'.format(response))
+print("\t- type: {}".format(type(response.text)))
 print('\t- content: {}'.format(response.text))
