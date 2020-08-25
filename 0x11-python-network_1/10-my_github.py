7a3c750d7e824a4ac85751759ee8f93ed5097855
dondropo <alejandroruscamoreno@gmail.com>
2020-08-25 04:39:30 +0000

    [Add files]: Task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+takes your Github credentials (username and password)
+and uses the Github API to display your id
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    response = requests.get('https://api.github.com/user', auth=(argv[1],
+                            argv[2]))
+    if "json" not in response.headers.get('content-type'):
+        print("Not a valid JSON")
+    else:
+        response = response.json()
+        print(response.get('id'))
+
