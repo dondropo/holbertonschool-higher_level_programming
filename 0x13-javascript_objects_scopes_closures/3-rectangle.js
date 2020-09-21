26c385572d8cda9d1de8c002c85ba588b4c7d7c4
dondropo <alejandroruscamoreno@gmail.com>
2020-09-21 17:24:41 +0000

    [Fix]: Logic in for loop

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -9,7 +9,7 @@ class Rectangle {
 
   print (w, h) {
     let i;
-    for (i = 0; i <= this.height; i++) {
+    for (i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
