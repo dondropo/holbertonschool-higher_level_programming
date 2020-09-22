5e4647cfd57888885d9e26459d28dfbe89fb1eb7
dondropo <alejandroruscamoreno@gmail.com>
2020-09-22 21:43:59 +0000

    [Update/Add] task 2 and 3 with correct use of request module

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-const request = require("request");
-request(process.argv[2], function(error, response, body) {
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  console.log("code:", response.statusCode);
+  console.log('code:', response.statusCode);
 });
