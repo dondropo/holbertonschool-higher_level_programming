59c370e2796cc62a7da203174bc973e8ec15a45a
dondropo <alejandroruscamoreno@gmail.com>
2020-09-23 04:02:38 +0000

    [Add files]: tasks 4 - 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+request(
+  {
+    url: url,
+    json: true
+  },
+  (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      if (response.statusCode === 200) {
+        const store = {};
+        body.forEach(element => {
+          const { userId, completed } = element;
+          if (completed) {
+            if (store[userId]) {
+              store[userId]++;
+            } else {
+              store[userId] = 1;
+            }
+          }
+        });
+        console.log(store);
+      }
+    }
+  }
+);
