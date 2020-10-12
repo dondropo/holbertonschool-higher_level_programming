28310c42baa1ea9b6a36ef2ff1f4225daaa8da71
dondropo <alejandroruscamoreno@gmail.com>
2020-10-12 18:17:53 +0000

    [Fix]

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,35 +1,20 @@
-#!/usr/bin/env node
+#!/usr/bin/node
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}/`;
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-request(
-  {
-    url: url,
-    json: true
-  },
-  (error, response, body) => {
-    if (error) {
-      console.log(error);
-    } else {
-      if (response.statusCode === 200) {
-        body.characters.forEach(element => {
-          request(
-            {
-              url: element,
-              json: true
-            },
-            (error, response, body) => {
-              if (error) {
-                console.log(error);
-              } else {
-                if (response.statusCode === 200) {
-                  console.log(body.name);
-                }
-              }
-            }
-          );
-        });
-      }
+request.get(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    for (const character of JSON.parse(body).characters) {
+      request.get(character, (err, response, body) => {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
     }
   }
-);
+});
